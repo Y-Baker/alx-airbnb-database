8bae40cbcbc13559bcef8086341b098ef20d0a6d
Y-Baker <yuossefahmedgaber@gmail.com>
2024-12-01 17:35:47 +0200

    Add Order By to query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,10 +6,12 @@ ON b.user_id = u.user_id;
 -- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 SELECT * FROM properties p
 LEFT JOIN reviews r
-ON p.property_id = r.property_id;
+ON p.property_id = r.property_id
+ORDER BY 
+    r.rating DESC,
+    p.name ASC;
 
 -- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT * FROM users u
 FULL OUTER JOIN bookings b
 ON u.user_id = b.user_id;
-
